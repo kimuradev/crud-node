94a91fc1ef5fafd1ccf4f201394e72f68abce1b1
Leandro Kimura <lkimura@scopus.com.br>
2017-01-02 16:27:19 -0200

    First commit

---
+++ server.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const app = express();
+
+var db;
+
+MongoClient.connect('mongodb://test:test@ds151048.mlab.com:51048/mongodb-quotes', (err, database) => {
+  // ... start the server
+	if (err) return console.log(err);
+  db = database;
+  app.listen(3000, () => {
+    console.log('listening on 3000');
+  })
+	console.log('conectado!!');
+})
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.get('/',(req,res) => {
+	res.sendFile(__dirname + '/index.html')
+});
+
+app.post('/quotes', (req, res) => {
+  db.collection('quotes').save(req.body, (err, result) => {
+    if (err) return console.log(err);
+
+    console.log('saved to database');
+    res.redirect('/')
+  })
+});
